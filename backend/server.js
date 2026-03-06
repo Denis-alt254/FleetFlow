8f9d296a069c0f27803e58981033a60bc445e255
Denis-alt254 <deniskipkurui2024@gmail.com>
2026-03-06 14:41:14 +0300

    adding cors and adding driver routes

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,20 @@
 const express = require('express');
+const cors = require('cors');
 const connectDB = require('./config/db');
+const router = require('./routers/DriverRouter');
 require('dotenv').config();
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.json());
 
 // connect to database
 connectDB();
+
+// Routes
+app.use('/api/drivers', router);
 
 app.listen(PORT, () => {
     console.log(`server is listening in http://localhost:${PORT}`);
